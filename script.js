8336130f7341e8fabb3b200e89908c10326e6128
Sahil Bose <bosesahil44@gmail.com>
2023-02-03 12:09:12 +0530

    v2.0.1.3

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 let header = document.querySelector('header');
 let menu = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
+let lightBar = document.querySelector('.light-bar');
+
+let custm_style = {
+    color: "#02A646"
+}
 
 menu.onclick = () => {
     navbar.classList.toggle('active');
@@ -22,9 +27,13 @@ darkmode.onclick =() => {
     if(darkmode.classList.contains('bx-moon')){
         darkmode.classList.replace('bx-moon', 'bx-sun');
         document.body.classList.add('active');
+        if(document.body.classList.contains('active')){
+            Object.assign(lightBar.style,custm_style);
+        }
     }
     else{
         darkmode.classList.replace('bx-sun', 'bx-moon');
         document.body.classList.remove('active');
     }
 }
+
